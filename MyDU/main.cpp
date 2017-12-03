3cdcbf93ccd51083a678afd842ec9238f962005f
antoine <antoine.rato@gmail.com>
2017-12-03 17:43:06 +0100

    First commit

---
+++ MyDU/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyDU
+//
+//  Created by Antoine Rato on 03/12/2017.
+//  Copyright © 2017 Antoine Rato. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello, World!\n";
+    return 0;
+}
