6569690243e128dc3871d3fde9c13c5953158496
antoine <antoine.rato@gmail.com>
2017-12-05 00:14:13 +0100

    Version of du -b

--- MyDU/main.cpp
+++ MyDU/main.cpp
@@ -22,14 +22,22 @@ int getSizeFoler(boost::filesystem::path folderPath)
     if(exists(folderPath))
     {
         if (is_regular_file(folderPath))
-            sizeFiles += file_size(folderPath);
+        {
+            sizeFiles += double(file_size(folderPath));
+        }
         else if (is_directory(folderPath))
         {
             directory_iterator end_itr;
             for (directory_iterator itr(folderPath); itr != end_itr; ++itr)
             {
-                if (is_regular_file(itr->path())) {
-                    sizeFiles += file_size(itr->path());
+                // cout << "file : " << itr->path().string() << endl;
+                if (is_regular_file(itr->path()))
+                {
+                    sizeFiles += double(file_size(itr->path()));
+                }
+                else if (is_directory(itr->path()))
+                {
+                    sizeFiles += getSizeFoler(itr->path());
                 }
             }
         }
@@ -39,8 +47,10 @@ int getSizeFoler(boost::filesystem::path folderPath)
         cout << folderPath << " does not exist\n";
     }
 
-    sizeFiles += 4;
-    cout << sizeFiles << endl;
+    sizeFiles += 4096;
+    cout << sizeFiles << "\t" << folderPath << endl;
+
+    return sizeFiles;
 }
 
 int main(int argc, const char * argv[])
@@ -51,62 +61,11 @@ int main(int argc, const char * argv[])
     // {
     //     if(!is_directory(*it))
     //     {
-    //         size += file_size(*it);
+    //         // size += file_size(*it);
             
-    //         std::cout << size << "\t" << *it << std::endl;
+    //         std::cout << file_size(*it) << "\t" << *it << std::endl;
     //     }
     // }
 
-    getSizeFoler("./dossier1");
-}
-
-// #include <iostream>
-#include <boost/filesystem.hpp>
-#include <boost/range/iterator_range.hpp>
-
-using namespace boost::filesystem;
-using namespace std;
-
-//returns a list of all the files in a given folder.
-std::vector<boost::filesystem::directory_entry> GetAllFilesInFolder(boost::filesystem::path folderPath)
-{
-    try
-    {
-        if (exists(folderPath))    // does p actually exist?
-        {
-            std::vector<boost::filesystem::directory_entry> files;
-
-            if (is_regular_file(folderPath))        // is p a regular file?
-                cout << folderPath << " size is " << file_size(folderPath) << '\n';
-
-
-
-            else if (is_directory(folderPath))      // is p a directory?
-            {
-                cout << folderPath << " is a directory containing:\n";
-                /*
-                copy(boost::filesystem::directory_iterator(folderPath), boost::filesystem::directory_iterator(),  // directory_iterator::value_type
-                    ostream_iterator<boost::filesystem::directory_entry>(cout, "\n"));  // is directory_entry, which is
-                                                                     // converted to a path by the
-                                                                     // path stream inserter
-                */
-                for (auto& entry : boost::make_iterator_range(boost::filesystem::directory_iterator(folderPath), {}))
-                {
-                    std::cout << entry << "\n";
-                    files.push_back(entry);
-                }
-                return files;
-            }
-        }
-        else
-        {
-            cout << folderPath << " does not exist\n";
-        }
-
-    }
-
-    catch (const boost::filesystem::filesystem_error& ex)
-    {
-        cout << ex.what() << '\n';
-    }
+    getSizeFoler("./");
 }
